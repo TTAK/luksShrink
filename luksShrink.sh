2837e2a6be01b1caeed90607fdced8a25c9d2c14
Arthur Besnard <abesnard@passageinnovation.com>
2020-09-18 17:22:53 +0200

    Removed -k and fixed some mistakes

--- luksShrink.sh
+++ luksShrink.sh
@@ -5,22 +5,19 @@ PROGNAME=$0
 
 usage() {
   cat << EOF >&2
-Usage: $PROGNAME -p <part> -s <size> [-b <keyfile>] [-v]
-
+Usage: $PROGNAME -p <part> -s <size>
 
 -p <part>: Partition containing the LUKS volume to resize
 -s <size>: New desired size for the LUKS volume
--b <keyfile>: setup a backup key from keyfile to the LUKS volume on slot 7
--v[v] : Verbose level none : -v : errors, -v : debug
+
 EOF
   exit 1
 }
 
-while getopts p:s:k o; do
+while getopts p:s o; do
 	case $o in
 		(p) part=$OPTARG;;
 		(s) newlukssize=$OPTARG;;
-		(k) backupkey=$OPTARG;;
 		(*) usage
 	esac
 done
@@ -44,16 +41,14 @@ getDisk()
 	echo `lsblk -no pkname $1`
 }
 
-echo "Selected disk : $disk   Selected partition : $part"
 #Checking if the disk and part are coherent
-disk=`getDisk $part` || giveup 2 
+disk=`getDisk $part` || giveup 2
 lsblk $disk || giveup 3
+echo "Selected disk : $disk   Selected partition : $part"
 
 
 echo "=>Oppening luks crypted volume"
 cryptsetup luksOpen "$part" cryptdisk && cryptopen=1 || giveup 4
-
-cryptsetup luksAddKey -S 7 "$part" ./BACKUP_KEY.txt || giveup 5
 
 echo "=>Getting gawk and python3"
 apt-get update && apt-get install -y gawk || giveup 6
@@ -66,14 +61,16 @@ sudo vgchange -ay $UBUNTU_VG && ubuntuvgopen=1 || giveup 7
 
 echo "=>resizing $UBUNTU_VG/root to $newlukssize"
 lvresize -L $newlukssize --resizefs $UBUNTU_VG/root || giveup 8
-SWAP_PV_POS=`pvs -v --segments /dev/mapper/cryptdisk | grep swap_1 | awk '{print $12}'`
 
+#We need a better way to defrag the LVM PV
+SWAP_PV_POS=`pvs -v --segments /dev/mapper/cryptdisk | grep swap_1 | awk '{print $12}'`
 echo "=>moving swap $SWAP_PV_POS"
 sudo pvmove --alloc anywhere "$SWAP_PV_POS" || giveup 9
 
 echo "=>checking vgroup health"
 e2fsck -f /dev/$UBUNTU_VG/root || giveup 10
 
+#We need a symplier and more reliable way to get these values
 echo "=>resizing LVM physical volume"
 PE_COUNT=`pvdisplay /dev/mapper/cryptdisk | grep "Allocated PE" | awk '{print $3}'`
 PE_SIZE=`pvdisplay /dev/mapper/cryptdisk | grep "PE Size" | awk '{print $3}' | cut -d. -f1`
@@ -82,6 +79,7 @@ NEW_SIZE=`gawk -M "BEGIN {print ((($PE_COUNT+2)*$PE_SIZE)+0$UNUSABLE_SIZE)*10485
 pvresize --setphysicalvolumesize "$NEW_SIZE"B /dev/mapper/cryptdisk || giveup 11
 echo "New size : $NEW_SIZE B"
 
+#We need a symplier and more reliable way to get these values
 echo "=>resizing LUKS volume : "
 PE_COUNT=`pvdisplay /dev/mapper/cryptdisk | grep Total | awk '{print $3}'`
 PE_SIZE=`pvdisplay /dev/mapper/cryptdisk | grep "PE Size" | awk '{print $3}' | cut -d. -f1`
@@ -90,17 +88,16 @@ NEW_LUKS_SECTOR_COUNT=`gawk -M "BEGIN {print ($PE_COUNT+1)*$PE_SIZE*1048576/$LUK
 cryptsetup -b $NEW_LUKS_SECTOR_COUNT resize cryptdisk || giveup 12
 echo "=>New LUKS sector count : $NEW_LUKS_SECTOR_COUNT sectors"
 
+#We need a symplier and more reliable way to get these values
 PARTITION_SECTOR_START=`parted $disk 'unit s print' | grep " 3 " | awk '{print $2}' | cut -ds -f1`
 LUKS_OFFSET_SECTORS=`cryptsetup status cryptdisk | grep "offset:" | awk '{print $2}'`
 NEW_PARTITION_SECTOR_END=`gawk -M "BEGIN {print $PARTITION_SECTOR_START+($NEW_LUKS_SECTOR_COUNT+$LUKS_OFFSET_SECTORS)-1}"`
-
 
 echo "=>Close LVM vgroup..."
 vgchange -a n $UBUNTU_VG && unset ubuntuvgopen || giveup 13
 
 echo "=>Closing LUKS volume..."
 cryptsetup close cryptdisk && unset cryptopen || giveup 14
-
 
 echo "=>Resizing partition to $NEW_PARTITION_SECTOR_END (please Enter the value $NEW_PARTITION_SECTOR_END)"
 parted $disk 'unit s resizepart 3' || giveup 15
